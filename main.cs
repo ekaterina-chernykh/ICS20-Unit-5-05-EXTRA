7791144de67a2d15dc8b0c267fe0a060b2b25e28
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-12-02 01:29:01 +0000

    whitespace?

--- main.cs
+++ main.cs
@@ -15,7 +15,7 @@ class Program
         double angleA;
         double angleB;
         double angleC;
-        double sumOfAllAngles
+        double sumOfAllAngles;
 
         // This function accepts user input
         Console.WriteLine("What type of triangle is it?");
@@ -37,23 +37,23 @@ class Program
         angleB = Math.Acos((Math.Pow(lengthC, 2) + Math.Pow(lengthA, 2) - Math.Pow(lengthB, 2)) / (2 * lengthC * lengthA)) * (180 / Math.PI);
         angleC = Math.Acos((Math.Pow(lengthA, 2) + Math.Pow(lengthB, 2) - Math.Pow(lengthC, 2)) / (2 * lengthA * lengthB)) * (180 / Math.PI);
 
-      sumOfAllAngles = angleA + angleB + angleC;
-      Console.WriteLine("Sum of all angles " + sumOfAllAngles.ToString("0.0"));
-      Console.WriteLine(" ");  
+        sumOfAllAngles = angleA + angleB + angleC;
+        Console.WriteLine("Sum of all angles " + sumOfAllAngles.ToString("0.0"));
+        Console.WriteLine(" " );  
 
-  if (angleA == angleB && angleA == angleC) {
-    Console.WriteLine("You have an equilateral triangle");
-  } else if (angleA != angleB && angleA != angleC && angleB != angleC) {
-    Console.WriteLine("You have an scalene triangle");
-  } else if (angleA != angleB && angleA == angleC) {
-    Console.WriteLine("You have an isosceles triangle");
-  } else if (angleA == angleB && angleA != angleC) {
-    Console.WriteLine("You have an isosceles triangle");
-  } else if (angleC == angleB && angleA != angleC) {
-    Console.WriteLine("You have an isosceles triangle");
-  } else {
-    Console.WriteLine("Not a triangle");
-  }
-    Console.WriteLine("\nDone.");
-  }
+  if (angleA == angleB && angleA == angleC); {
+      Console.WriteLine("You have an equilateral triangle");
+    } else if (angleA != angleB && angleA != angleC && angleB != angleC); {
+      Console.WriteLine("You have an scalene triangle");
+    } else if (angleA != angleB && angleA == angleC); {
+      Console.WriteLine("You have an isosceles triangle");
+    } else if (angleA == angleB && angleA != angleC); {
+      Console.WriteLine("You have an isosceles triangle");
+    } else if (angleC == angleB && angleA != angleC); {
+      Console.WriteLine("You have an isosceles triangle");
+    } else {
+      Console.WriteLine("Not a triangle");
+    }
+      Console.WriteLine("\nDone.");
+    }
 }
